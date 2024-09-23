c8f3e2188922cc7a998ede63fa56c1c0ef6f6c45
DBaldwin8 <darren_baldwin@live.com>
2024-09-23 11:03:33 +0100

    var_dump working in php file for db connection

--- paint-collector-app.php
+++ paint-collector-app.php
@@ -1,1 +1,22 @@
 <?php
+
+$db = new PDO (
+    'mysql:host=DB;dbname=collector_app',
+    'root',
+    'password'
+);
+
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$query = $db->prepare ('SELECT * FROM `paints`');
+
+$result = $query->execute();
+
+if (!isset($result)) {
+    return 'You broke it';
+} else {
+    $table = $query->fetchAll();
+    echo '<pre>';
+    var_dump($table);
+    echo '</pre>';
+}
