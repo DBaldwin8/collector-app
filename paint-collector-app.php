2575f636d22fd0484fbc9daef65c74089b9b0041
DBaldwin8 <darren_baldwin@live.com>
2024-09-23 13:41:40 +0100

    refactored query to use names of fields

--- paint-collector-app.php
+++ paint-collector-app.php
@@ -8,7 +8,7 @@ $db = new PDO (
 
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-$query = $db->prepare ('SELECT * FROM `paints`');
+$query = $db->prepare ('SELECT `brand`, `name`, `color`, `base`, `quantity_left`, `purchase_date` FROM `paints`');
 
 $result = $query->execute();
 
